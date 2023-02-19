0fbcd43b6ec14e0a351db88c153cbc77b9d7183e
Katie A <katie.t.abraham@gmail.com>
2023-02-19 14:05:20 -0600

    setting up basic component

---
+++ src/views/TheDashboard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import BaseCard from '../components/BaseCard.vue'
+
+export default {
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        <BaseCard />
+    </main>
+</template>
