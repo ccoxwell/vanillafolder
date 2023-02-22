11efcf55fc591a13e7ef52584fc70cbac51363a5
Katie A <katie.t.abraham@gmail.com>
2023-02-22 11:23:51 -0600

    adding cards

--- src/main.js
+++ src/main.js
@@ -6,8 +6,7 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-
-import "./assets/main.css";
+import VCalendar from 'v-calendar';
 
 const vuetify = createVuetify({
   components,
@@ -18,5 +17,5 @@ const app = createApp(App);
 
 // app.use(router);
 app.use(vuetify);
-
+app.use(VCalendar, {})
 app.mount("#app");
