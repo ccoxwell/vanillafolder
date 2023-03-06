976eb7d7c67a0cc981fca6b324e647ab202181e1
Katie A <katie.t.abraham@gmail.com>
2023-03-06 09:51:37 -0600

    adding data to dashboard

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,6 @@
 
 <script setup>
 import TheDashboard from './views/TheDashboard.vue'
-import { ref, computed } from 'vue'
-
-const userList = ref([])
-
-fetchMockData()
-
-async function fetchMockData() {
-  const response = await fetch('http://localhost:3004/')
-  const usersObject = response.json()
-  const users = { usersObject }
-  userList.value = users
-}
 </script>
 
 <style scoped></style>
--- src/views/TheDashboard.vue
+++ src/views/TheDashboard.vue
@@ -1,47 +1,59 @@
-<script setup>
-import 'v-calendar/dist/style.css';
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card title="User Info">
+          <v-card-text>
+            <p>Name: {{ currentUser.firstName }} {{ currentUser.lastName }}</p>
+            <p>Address: {{ currentUser.address }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card title="Emergency Info">
+          <v-card-text>
+            <p>Allergies: {{ currentUser.allergies }}</p>
+            <p>Emergency Contact: {{ currentUser.emergencyContact }}</p>
+            <p>Medications: {{ currentUser.medicationList }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card title="Vault">
+          <v-card-text> This will be a grid of PDFs at some point </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card title="Calendar">
+          <v-date-picker v-model="date" />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
+<script setup>
+import { ref, computed } from 'vue'
+import "v-calendar/dist/style.css";
+
+const USER_ID = "63fb7794a745b07e9fa6a45b"
+
+const users = ref([])
+
+const currentUser = computed(() => users.value.find(user => user._id == USER_ID))
+
+getUserList()
+
+function getUserList() {
+  fetchUsers().then(response => {
+    users.value = response
+  })
+}
+
+async function fetchUsers() {
+  const response = await fetch("http://localhost:3004/users")
+  const body = response.json();
+  return body;
+}
 
 </script>
-
-<template>
-    <v-container>
-        <v-row>
-            <v-col cols="12">
-        <v-card 
-        title="User Info"
-        >
-            <v-card-text>
-               <p>Name: </p>
-               <p>Address: </p>
-            </v-card-text>
-        </v-card>
-    </v-col>
-    <v-col cols="12">
-        <v-card
-        title="Emergency Info">
-            <v-card-text>
-                <p>Allergies:</p>
-                <p>Medical Issues:</p>
-                <p>Emergency Contact:</p>
-                <p>Medications:</p>
-            </v-card-text>
-        </v-card>
-    </v-col>
-    <v-col cols="12">
-        <v-card
-        title="Vault">
-            <v-card-text>
-                This will be a grid of PDFs at some point
-            </v-card-text>
-        </v-card>
-    </v-col>
-    <v-col cols="12">
-        <v-card
-        title="Calendar">
-            <v-date-picker v-model="date" />
-        </v-card>
-    </v-col>
-        </v-row>
-    </v-container>
-</template>
